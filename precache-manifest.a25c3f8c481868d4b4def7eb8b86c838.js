639155f60ca32f749670281029f9001c5f5ab704
Tymoteusz Wojcik <WojcikTymoteusz9@gmail.com>
2020-07-23 21:46:46 +0200

    Updates

---
+++ precache-manifest.a25c3f8c481868d4b4def7eb8b86c838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70b8a67a112c470221fe3a785702ace",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "f1902aba7c7a62243984",
+    "url": "/todos-list-react/static/css/main.32a918f2.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1902aba7c7a62243984",
+    "url": "/todos-list-react/static/js/main.88a79412.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  }
+]);
